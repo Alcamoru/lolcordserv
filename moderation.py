6c179ae4078a466301e9cb41abda1844bdf63d56
Alexis <alcamoru@icloud.com>
2023-06-18 18:47:17 +0200

    Added roles distribution

--- moderation.py
+++ moderation.py
@@ -16,12 +16,23 @@ class Moderation(commands.Cog):
                                                     url="https://discord.com/channels/1117482753076776982/1119546314007519302",
                                                     )
             self.add_item(announcement_button)
+            self.button_callback(announcement_button, )
             self.bot: commands.Bot = bot
 
         async def button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
             response: discord.InteractionResponse = interaction.response
             rules_channel = self.bot.get_channel(1119542979896557599)
             await response.send_message("Test")
+
+    @commands.slash_command(name="sendembed")
+    async def sendembed(self, ctx: discord.ApplicationContext):
+        embed = discord.Embed(title="Bienvenue sur Lolcord", description="Attribuez vous des rôles")
+        embed.add_field(name="✅ Membre", value="Pour accéder aux principaux salons du serveur")
+        embed.add_field(name="🎮 LOLEUR", value="Vous jouez à League of Legends et souhaitez accéder aux salons de LOL",
+                        inline=False)
+        channel: discord.TextChannel = self.bot.get_channel(1119546314007519302)
+        await channel.send(embed=embed)
+
 
     @commands.slash_command(name="button")
     async def button(self, ctx: discord.ApplicationContext):
@@ -35,14 +46,38 @@ class Moderation(commands.Cog):
         await ctx.send("**Bip Boup** \n Je suis le maitre de ce serveur \n"
                        "**Bip Boup**", view=self.AnnouncementsButton(self.bot), embed=rules_embed)
 
-    @commands.Cog.listener()
-    async def on_raw_raction_add(self, payload: discord.RawReactionActionEvent):
-        user = self.bot.get_user(payload.user_id)
-        if payload.emoji: discord.Emoji == discord.Emoji()
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         message_content = message.content
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        guild = self.bot.get_guild(1117482753076776982)
+        user: discord.Member = guild.get_member(payload.user_id)
+
+        if payload.message_id == 1119618619404468236:
+            if payload.emoji.name == "✅":
+                member_role: discord.Role = guild.get_role(1119559385820176404)
+                await user.add_roles(member_role)
+            if payload.emoji.name == "🎮":
+                loleur_role = guild.get_role(1117486329446531195)
+                await user.add_roles(loleur_role)
+
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+        guild = self.bot.get_guild(1117482753076776982)
+        user: discord.Member = guild.get_member(payload.user_id)
+
+        if payload.message_id == 1119618619404468236:
+            if payload.emoji.name == "✅":
+                member_role: discord.Role = guild.get_role(1119559385820176404)
+                await user.remove_roles(member_role)
+            if payload.emoji.name == "🎮":
+                loleur_role = guild.get_role(1117486329446531195)
+                await user.remove_roles(loleur_role)
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Moderation(bot))
